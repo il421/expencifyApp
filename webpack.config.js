cf91f2969a9ef6c53bcd00182cb3a0b4ec0169c1
Ilya Suglobov <il421@mail.ru>
2019-01-09 23:08:32 +1300

    09/01 Prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,36 @@
 // make a path for outputs
 const path = require('path');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
+module.exports = (env) => {
+  console.log('env', env);
+  const isProduction = env === 'production';
 
-  module: { // to convers es6 via babel
-    rules: [{
-      loader: 'babel-loader', // how
-      test: /\.js$/, // what
-      exclude: /node_modules/ // not
-    }, {
-      test: /\.s?css$/, // CSS and SCSS
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-
-  devtool: 'cheap-module-eval-source-map', // to detect errors inside modeles
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true // tell server that we are going to tout via a client side
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+  
+    module: { // to convers es6 via babel
+      rules: [{
+        loader: 'babel-loader', // how
+        test: /\.js$/, // what
+        exclude: /node_modules/ // not
+      }, {
+        test: /\.s?css$/, // CSS and SCSS
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+  
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map', // to detect errors inside modeles
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true // tell server that we are going to tout via a client side
+    }
   }
 };
