0ce436b6caf09b2d29e540bfda817efe045d0701
Ilya Suglobov <il421@mail.ru>
2019-02-25 22:53:52 +1300

    25/02 Added provate routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import { startSetExpenses } from './actions/expenses';
 import configStore from './store/configStore';
+import { login, logout } from './actions/auth';
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 
 const store = configStore();
@@ -19,9 +20,30 @@ const jsx = ( // Provider - Now we can use store everywhere
   </Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+};
+
 ReactDOM.render(<p>Loading ...</p>, document.getElementById('app'));
 
-// set data than render
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
-});
+// google authification
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch(login(user.uid));
+    // set data than render
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+  } else {
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
+  }
+})
